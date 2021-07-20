c94164088c36fb951db2c9715fc50aa4d964571b
katredford <katharine.redford@gmail.com>
2021-07-20 12:48:42 -0500

    import inquirer in index

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-const path = require('path');
-const fs = require('fs');
-const createPage = require('template')
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const createPage = require("template");
 
 let group = [];
 
 const promptUser = () => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?'
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email?'
-        },
-        {
-            type: 'input',
-            name: 'officeNumber',
-            message: 'What is your office number?'
-        }
+	inquirer.prompt([
+		{
+			type: "input",
+			name: "name",
+			message: "What is your name?",
+		},
+		{
+			type: "input",
+			name: "email",
+			message: "What is your email?",
+		},
+		{
+			type: "input",
+			name: "officeNumber",
+			message: "What is your office number?",
+		},
+	]);
+};
